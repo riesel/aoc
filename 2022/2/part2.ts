6421323c8d9feb4ad65f4839fdc586298421cdac
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2022-12-02 08:02:24 +0100

    Cleanup

--- 2022/2/part2.ts
+++ 2022/2/part2.ts
@@ -1,4 +1,4 @@
-import { printMatrix, splitLines, sum } from "../../utils"
+import { splitLines, sum } from "../../utils"
 
 const scoreMap = {
   "A X": 0 + 3, "A Y": 3 + 1, "A Z": 6 + 2,
@@ -6,7 +6,9 @@ const scoreMap = {
   "C X": 0 + 2, "C Y": 3 + 3, "C Z": 6 + 1,
 }
 
+type Variant = keyof typeof scoreMap
+
 export default function (rawData: string) {
-  const scores = splitLines(rawData).map(line => scoreMap[line])
+  const scores = splitLines(rawData).map(line => scoreMap[line as Variant])
   return sum(scores)
 }
